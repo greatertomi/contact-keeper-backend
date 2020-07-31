d65e32f8f58e19ba5ce1a3f657b35c4a36abb86b
Oshalusi John <oshalusijohn@gmail.com>
2020-07-31 18:32:41 +0100

    Finished working with the auth

---
+++ middlewares/validators.js
@@ -0,0 +1,16 @@
+const { body } = require('express-validator');
+exports.loginValidation = [
+  body('email', 'Please include a valid email').isEmail(),
+  body('password', 'Password is required').exists()
+];
+
+exports.createUserValidation = [
+  body('name', 'Please add name').not().isEmpty(),
+  body('email', 'Please include a valid email').isEmail(),
+  body(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({
+    min: 5
+  })
+];
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
 
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/contacts', require('./routes/contacts'));
+// Used to parse json request. You would formerly use body-parser for this.
+app.use(express.json({ extended: false }));
 
+app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/auth', require('./routes/auth'));
+app.use('/api/v1/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
